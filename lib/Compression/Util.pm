cc4dc6389e9da9d772cd543b020a7733cf5a3bf9
trizen <trizenx@tutanota.com>
2024-08-25 21:18:26 +0300

    Version 0.13

--- lib/Compression/Util.pm
+++ lib/Compression/Util.pm
@@ -9,7 +9,7 @@ require Exporter;
 
 our @ISA = qw(Exporter);
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 our $VERBOSE = 0;        # verbose mode
 
 our $LZ_MIN_LEN       = 4;          # minimum match length in LZ parsing
